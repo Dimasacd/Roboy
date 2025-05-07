2b7b88e4a63205e422db7a089f8f4af304e41906
Dimasacd <vitor.dimas.dev@outlook.com>
2025-05-07 18:17:28 -0300

    Roboy is my favorite accident

--- FirstImpact.py
+++ FirstImpact.py
@@ -1,9 +1,17 @@
-print ('Hello Wolrd')
-print ('(Hello World) is my first script in my life')
-print ('Roboy is second script...')
+print('Hello World')
+print('(Hello World) is my first script in my life')
+print('Roboy is second script...')
 
-print ('Roboy? is time!')
+print("Roboy? it's time!")
 
-print ("Roboy: Hello world, my name is Roboy, the robot.")
-print ("Your first robot friend — or maybe your first robot friend was my buddy ChatGPT? ")
-print ("Either way, what's your name, partner?")
+print("Roboy: Hello world, my name is Roboy, the robot.")
+print("Your first robot friend — or maybe your first robot friend was my buddy ChatGPT? ")
+print("Either way, what's your name?")
+
+name = input('Your Name:')
+
+print('Hello {}!'.format(name))
+print("{} You know my skills? I very good in calculation".format(name))
+
+
+
