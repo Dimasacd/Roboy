e9f7182c9ee0be454aabdf0f68dd0ea2df515e6d
Dimasacd <vitor.dimas.dev@outlook.com>
2025-05-07 17:25:34 -0300

    First Impact Project
    First Impact is my personal project in Python.
    
     focused on training and registration

---
+++ FirstImpact.py
@@ -0,0 +1,9 @@
+print ('Hello Wolrd')
+print ('(Hello World) is my first script in my life')
+print ('Roboy is second script...')
+
+print ('Roboy? is time!')
+
+print ("Roboy: Hello world, my name is Roboy, the robot.")
+print ("Your first robot friend — or maybe your first robot friend was my buddy ChatGPT? ")
+print ("Either way, what's your name, partner?")
